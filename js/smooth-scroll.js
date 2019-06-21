39f6a00ed140d49ab92f509d54abdbc8b349b8ff
Umer Khan <umerkk164@gmail.com>
2019-06-21 19:34:33 +0300

    Initial Commit

---
+++ js/smooth-scroll.js
@@ -0,0 +1,20 @@
+$('#cover .navButton').on('click', function (event) {
+    event.preventDefault();
+    let scrollPos;
+    if (event.currentTarget.name == "next") {
+        // const hash = "#f" + (parseInt(event.currentTarget.parentNode.parentNode.parentNode.id.slice(1)) + 1);
+        scrollPos = $('#cover').scrollTop() + $('#cover').height();
+    } else {
+        scrollPos = $('#cover').scrollTop() - $('#cover').height();
+        // const hash = "#f" + (parseInt(event.currentTarget.parentNode.parentNode.parentNode.id.slice(1)) - 1);
+    }
+
+
+    $('#cover').animate(
+        {
+            scrollTop: scrollPos - scrollPos%$("#cover").height()
+            // scrollTop: $(hash).offset().top
+        },
+        500
+    );
+});
